0a68455a04471b3464f913ccdd3bbe739cc38846
Robbie Cahill <robbie-cahill@proton.me>
2023-05-24 20:30:24 +1000

    Add some help text

--- bin/serve-public.ts
+++ bin/serve-public.ts
@@ -9,6 +9,17 @@ const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 process.title = "serve-public";
 
 program.name("serve-public")
+    .version('1.0.0')
+    .usage(`
+serve-public
+serve-public [--path <path>] [--port <port>] [--domain <domain>]
+
+Serve a local folder with a public URL.
+
+With no arguments the folder will be the current folder, the port will be 3000 and you'll be using Tunnelmole in free mode with a random subdomain.
+
+Otherwise pass in --path to change the folder, --port to change the port and/or --domain to use a custom Tunnelmole subdomain (requires a paid subscription from https://dashboard.tunnelmole.com)
+    `)
     .option("--port <port>", "The port to listen on locally. Defaults to port 3000")
     .option("--path <path>", "The path to your folder containing your files. Defaults to the current folder")
     .option("--domain <domain>", "Tunnelmole domain e.g. mysite.tunnelmole.com. Requires a paid subscription from https://dashboard.tunnelmole.com")

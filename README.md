808dc000e1a26998bc0d1a672c78fb1945e3d683
Robbie Cahill <robbie-cahill@proton.me>
2023-05-30 18:35:32 +1000

    Add support to serve directories

---
+++ README.md
@@ -0,0 +1,14 @@
+### Serve Public
+Serve any folder on your device with a Public URL
+
+#### Installation
+Install `serve-public` with NPM.
+
+Run `npm install -g serve-public`
+
+#### Usage
+Get a Public URL for any folder on your device with:
+`serve-public --path </path/to/your/folder>`
+
+
+#### How it works
--- src/serve-public.ts
+++ src/serve-public.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import tunnelmole from "tunnelmole/cjs";
+import serveIndex from "serve-index";
 
 const servePublic = async function (port: number, path: string, domain?: string) {
     const app = express();
     app.use(express.static(path));
+    app.use(serveIndex(path));
     console.info(`Local server listening on http://localhost:${port}`);
     await tunnelmole({
         port,
